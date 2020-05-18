0cf77d8bc98635a5282921eb5a99824cd224e757
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-18 11:54:57 +0900

    Revert "型を変更しない"
    This reverts commit 17c56b1b73c62e8ad39678a0afce446667a552e0.

--- main.php
+++ main.php
@@ -1,20 +1,13 @@
 <?php
 
-$a = 'hello';
-$b = 10;
-$c = -1.3;
-$d = null;
-$e = true;
+// $name = 'taguchi';
+// $name = 'dotinstall';
 
-// var_dump($a);
-// var_dump($b);
-// var_dump($c);
-// var_dump($d);
-// var_dump($e);
+// define('NAME', 'taguchi');
+// define('NAME', 'dotinstall');
+// echo NAME . PHP_EOL;
 
-$x = (float)5;
-$y = (string)3.2;
-
-var_dump($x);
-var_dump($y);
+const NAME = 'taguchi';
+const NAME = 'dotinstall';
+echo NAME . PHP_EOL;
 ?>
