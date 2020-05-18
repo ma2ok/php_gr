17c56b1b73c62e8ad39678a0afce446667a552e0
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-18 11:54:46 +0900

    型を変更しない

--- main.php
+++ main.php
@@ -1,13 +1,20 @@
 <?php
 
-// $name = 'taguchi';
-// $name = 'dotinstall';
+$a = 'hello';
+$b = 10;
+$c = -1.3;
+$d = null;
+$e = true;
 
-// define('NAME', 'taguchi');
-// define('NAME', 'dotinstall');
-// echo NAME . PHP_EOL;
+// var_dump($a);
+// var_dump($b);
+// var_dump($c);
+// var_dump($d);
+// var_dump($e);
 
-const NAME = 'taguchi';
-const NAME = 'dotinstall';
-echo NAME . PHP_EOL;
+$x = (float)5;
+$y = (string)3.2;
+
+var_dump($x);
+var_dump($y);
 ?>
