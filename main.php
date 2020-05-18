665d819ecd815712ccd90613f08cda130a37ac43
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-18 11:06:40 +0900

    ドキュメント

--- main.php
+++ main.php
@@ -2,8 +2,14 @@
 
 $name = 'taguchi';
 
-// echo 'It's Sunday. Hello ' . $name . PHP_EOL;
-// echo "It's Sunday. Hello " . $name . PHP_EOL;
-// echo "It's Sunday. Hello $name" . PHP_EOL;
-// echo "It's \"Sunday\". Hello $name" . PHP_EOL;
-echo "It's \"Sunday\".\t Hello $name" . PHP_EOL;
+// $text = <<<'EOT' // nowdoc
+// $text = <<<"EOT" // heredoc
+  $text = <<<EOT
+  hello! $name
+    this is looooong
+  text!
+  
+  EOT;
+
+echo $text;
+?>
