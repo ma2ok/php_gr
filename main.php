b58ea598ad5ab93b51cced02f52d00cedb30a272
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-18 10:55:20 +0900

    変数のやり方

--- main.php
+++ main.php
@@ -1,13 +1,11 @@
 <?php
 
-// comment
-# comment
-/* 
-comment
-comment
-comment
-*/
+// $name = 'taguchi';
+$name = 'dotinstall';
 
-echo 'Hello taguchi' . PHP_EOL;
+// echo 'Hello taguchi' . PHP_EOL;
 // echo 'Hi taguchi' . PHP_EOL;
-?>
+
+echo 'Hello ' . $name . PHP_EOL;
+// echo 'Hi ' . $name . PHP_EOL;
+echo 'Hi ' . $Name . PHP_EOL;
