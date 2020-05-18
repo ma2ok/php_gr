e78c0325b56459808ccc58916e5de18ebc0eca8e
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-18 12:29:18 +0900

    For文

--- main.php
+++ main.php
@@ -1,23 +1,6 @@
 <?php
-
-$signal = 'red';
-// $signal = 'blue';
-// $signal = 'green';
-// $signal = 'pink';
-
-switch ($signal) {
-  case 'red':
-    echo 'Stop!' . PHP_EOL;
-    // break;
-  case 'yellow':
-    echo 'Caution!' . PHP_EOL;
-    break;
-  case 'blue':
-  case 'green':
-    echo 'Go!' . PHP_EOL;
-    break;
-  default:
-    echo 'Wrong signal!!' . PHP_EOL;
-    break;
+for ($i = 1; $i <= 5; $i++) {
+  // echo 'Hello' . PHP_EOL;
+  echo "$i - Hello" . PHP_EOL;
 }
 ?>
