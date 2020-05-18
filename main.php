98a83cf50c6382f7bd241a8c2da7bef32bdc7712
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-18 12:24:59 +0900

    Switch文の詳細な使い方

--- main.php
+++ main.php
@@ -1,25 +1,23 @@
 <?php
 
-// $signal = 'red';
-$signal = 'blue';
-
-// if ($signal === 'red') {
-//   echo 'Stop!' . PHP_EOL;
-// } elseif ($signal === 'yellow') {
-//   echo 'Caution!' . PHP_EOL;
-// } elseif ($signal === 'blue'){
-//   echo 'Go!' . PHP_EOL;
-// }
+$signal = 'red';
+// $signal = 'blue';
+// $signal = 'green';
+// $signal = 'pink';
 
 switch ($signal) {
   case 'red':
     echo 'Stop!' . PHP_EOL;
-    break;
+    // break;
   case 'yellow':
     echo 'Caution!' . PHP_EOL;
     break;
   case 'blue':
+  case 'green':
     echo 'Go!' . PHP_EOL;
+    break;
+  default:
+    echo 'Wrong signal!!' . PHP_EOL;
     break;
 }
 ?>
