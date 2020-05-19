560c97f7c4360f1bdc545b7682dd26b59dfd24ee
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-19 10:36:58 +0900

    return

--- main.php
+++ main.php
@@ -1,18 +1,14 @@
 <?php
 
-function showAd($message = 'Ad')  // 仮引数
+function sum($a, $b, $c)
 {
-  echo '----------' . PHP_EOL;
-  echo '--- ' . $message . ' ---' . PHP_EOL;
-  echo '----------' . PHP_EOL;
+  // echo $a + $b + $c . PHP_EOL;
+  return $a + $b + $c;
+  echo 'Here' . PHP_EOL;
 }
 
-showAd('Header Ad'); // 実引数
-echo 'Tom is great!' . PHP_EOL;
-echo 'Bob is great!' . PHP_EOL;
-// showAd('Ad');
-showAd();
-echo 'Steve is great!' . PHP_EOL;
-echo 'Bob is great!' . PHP_EOL;
-showAd('Footer Ad');
+// sum(100, 200, 300); // 600
+// sum(300, 400, 500); // 1200
+
+echo sum(100, 200, 300) + sum(300, 400, 500) . PHP_EOL; // 1800
 ?>
