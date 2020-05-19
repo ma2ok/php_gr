a7783168d0d8db3ac712ab839cdc3d8a0b95a928
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-19 14:39:20 +0900

    可変長引数

--- main.php
+++ main.php
@@ -1,19 +1,16 @@
 <?php
 
-$moreScores = [
-  55,
-  72,
-  'perfect',
-  [90, 42, 88],
-];
+// function sum($a, $b, $c)
+function sum(...$numbers)
+{
+  // return $a + $b + $c;
+  $total = 0;
+  foreach ($numbers as $number) {
+    $total += $number;
+  }
+  return $total;
+}
 
-$scores = [
-  90,
-  40,
-  100,
-];
-
-// print_r($scores);
-
-echo $scores[5][2] . PHP_EOL;
+echo sum(1, 3, 5) . PHP_EOL;
+echo sum(4, 2, 5, 1) . PHP_EOL;
 ?>
