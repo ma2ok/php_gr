6e5da05f790d6c59f042cb802d03b3d0083447e9
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-19 10:50:12 +0900

    スコープ

--- main.php
+++ main.php
@@ -1,14 +1,13 @@
 <?php
 
+$rate = 1.1; // グローバルスコープ
+
 function sum($a, $b, $c)
 {
-  // echo $a + $b + $c . PHP_EOL;
-  return $a + $b + $c;
-  echo 'Here' . PHP_EOL;
+  // global $rate;
+  $rate = 1.08; // ローカルスコープ
+  return ($a + $b + $c) * $rate;
 }
 
-// sum(100, 200, 300); // 600
-// sum(300, 400, 500); // 1200
-
-echo sum(100, 200, 300) + sum(300, 400, 500) . PHP_EOL; // 1800
+echo sum(100, 200, 300) + sum(300, 400, 500) . PHP_EOL; // 1944
 ?>
