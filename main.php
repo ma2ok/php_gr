434d0f7f9abeee02d5739a3fd132c9761df1e605
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-19 10:33:39 +0900

    引数

--- main.php
+++ main.php
@@ -1,17 +1,18 @@
 <?php
 
-function showAd() 
+function showAd($message = 'Ad')  // 仮引数
 {
   echo '----------' . PHP_EOL;
-  echo '--- Ad ---' . PHP_EOL;
+  echo '--- ' . $message . ' ---' . PHP_EOL;
   echo '----------' . PHP_EOL;
 }
 
-showAd();
+showAd('Header Ad'); // 実引数
 echo 'Tom is great!' . PHP_EOL;
 echo 'Bob is great!' . PHP_EOL;
+// showAd('Ad');
 showAd();
 echo 'Steve is great!' . PHP_EOL;
 echo 'Bob is great!' . PHP_EOL;
-showAd();
+showAd('Footer Ad');
 ?>
