2b6a9e1075648b3717ea54173082c23288803c66
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-19 14:28:35 +0900

    配列をキー指定

--- main.php
+++ main.php
@@ -1,15 +1,13 @@
 <?php
 
-// $score1 = 90;
-// $score2 = 40;
-// $score3 = 100;
-
 $scores = [
-  90,
-  40,
-  100,
+  'first'  => 90, 
+  'second' => 40, 
+  'third'  => 100,
 ];
 
-$scores[1] = 60;
-echo $scores[1] . PHP_EOL;
+// var_dump($scores);
+// print_r($scores);
+
+echo $scores['third'] . PHP_EOL;
 ?>
