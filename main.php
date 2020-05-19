28cd8c6ba4334cae0117347d9de088c591635b15
Tubasa Matsuoka <workwing0283@gamil.com>
2020-05-19 10:13:45 +0900

    continueとかbreakとか

--- main.php
+++ main.php
@@ -1,15 +1,13 @@
 <?php
 
-// $hp = 100;
-$hp = -50;
-
-// while ($hp > 0) {
-//   echo "Your HP: $hp" . PHP_EOL;
-//   $hp -= 15;
-// }
-
-do {
-  echo "Your HP: $hp" . PHP_EOL;
-  $hp -= 15;
-} while ($hp > 0);
+for ($i = 1; $i <= 10; $i++) {
+  // if ($i === 4) {
+  // if ($i % 3 === 0) {
+  //   continue;
+  // }
+  if ($i === 4) {
+    break;
+  }
+  echo $i . PHP_EOL;
+}
 ?>
